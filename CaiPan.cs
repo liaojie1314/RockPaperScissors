f72730063b03a5b04a2897b43881f70b96bcb008
liaojie1314 <1517438366@qq.com>
2022-02-04 19:47:07 +0800

    C#剪刀石头布窗体应用程序

---
+++ CaiPan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 石头剪刀布
+{
+    public enum Result
+    {
+        玩家赢,
+        电脑赢,
+        平局
+    }
+    class CaiPan
+    {
+        public static Result Judge(int playerNumber,int computerNumber)
+        {
+            if(playerNumber-computerNumber==-1||playerNumber-computerNumber==2)
+            {
+                return Result.玩家赢;
+            }else if(playerNumber-computerNumber==0)
+            {
+                return Result.平局;
+            }
+            else
+            {
+                return Result.电脑赢;
+            }
+        }
+    }
+}
---
+++ Computer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 石头剪刀布
+{
+    class Computer
+    {
+        public string Fist
+        {
+            get;
+            set;
+        }
+        public int ShowFist()
+        {
+            Random r = new Random();
+            int rNumber = r.Next(1, 4);
+            switch(rNumber)
+            {
+                case 1:
+                    this.Fist = "石头";
+                    break;
+                case 2:
+                    this.Fist = "剪刀";
+                    break;
+                case 3:
+                    this.Fist = "布";
+                    break;
+            }
+            return rNumber;
+        }
+    }
+}
